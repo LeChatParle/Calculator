b78330892e328d25424b990c8eb3d05455479b1c
Jeremy Edwards <edwardsbjeremy@gmail.com>
2018-04-13 16:04:37 -0500

    Initial Commit

---
+++ Calculator Extreme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator Extreme
+//
+//  Created by Jeremy Edwards on 2018-04-13.
+//  Copyright © 2018 Jeremy Edwards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
